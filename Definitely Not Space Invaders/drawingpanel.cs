357bd8937f8d5571cc94f272caeb665984b6f288
David Berry <Bioniclegenius@gmail.com>
2015-07-13 15:21:24 -0500

    Initial Commit
    Added the drawingpanel class, which handles all rendering. Everything
    must route through it for handling input AND output. Also added stars to
    make a nice background for the project.

---
+++ Definitely Not Space Invaders/drawingpanel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Definitely_Not_Space_Invaders {
+  public class drawingpanel:Panel {
+    public List<star> stars;
+    public System.Diagnostics.Stopwatch st=new System.Diagnostics.Stopwatch();
+    public long lasttime;
+    public drawingpanel(int dimx=100,int dimy=100) {
+      this.Size=new Size(dimx,dimy);
+      this.Location=new Point(0,0);
+      this.DoubleBuffered=true;
+      this.Paint+=new System.Windows.Forms.PaintEventHandler(this.PaintEvent);
+      this.MouseMove+=new System.Windows.Forms.MouseEventHandler(this.MouseMoveEvent);
+      stars=new List<star>();
+      for(int x=0;x<70.0*dimx/100;x++)
+        stars.Add(new star(dimx,dimy));
+      st.Start();
+      lasttime=st.ElapsedMilliseconds;
+      Invalidate();
+    }
+    public void PaintEvent(object sender,PaintEventArgs e) {
+      SolidBrush b=new SolidBrush(Color.FromArgb(0,0,0));
+      Graphics g=e.Graphics;
+      long time=st.ElapsedMilliseconds-lasttime;
+      lasttime+=time;
+      if(time!=0) {
+        g.FillRectangle(b,0,0,this.Width,this.Height);
+        for(int x=0;x<stars.Count;x++)
+          stars[x].paint(g,time);
+      }
+      Invalidate();
+    }
+    public void MouseMoveEvent(object sender,MouseEventArgs m) {
+      int mouseX=m.X;
+      int mouseY=m.Y;
+      //do whatever you so wish with this information, the mouse X and Y coordinates
+    }
+  }
+}
---
+++ Definitely Not Space Invaders/star.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Definitely_Not_Space_Invaders {
+  public class star {
+    public double x,y;
+    public int size;
+    public static Random r=new Random();
+    public int scrWidth,scrHeight;
+    public star(int width,int height) {
+      scrWidth=width;
+      scrHeight=height;
+      x=r.Next(0,2*width);
+      y=r.Next(0,height);
+      size=r.Next(1,11);
+    }
+    public void paint(Graphics g,long mspassed=0) {
+      SolidBrush b;
+      double basespeed=.020;
+      double speedfactor=.003;
+      double curspeed=basespeed+(size-1)*speedfactor;
+      x-=curspeed*mspassed;
+      if(x<-5) {
+        x=r.Next(scrWidth,scrWidth*2);
+        y=r.Next(0,scrHeight);
+        size=r.Next(1,11);
+      }
+      for(int z=0;z<(int)(size/2.0+.5);z++) {
+        b=new SolidBrush(Color.FromArgb(196-32*z,196-32*z,196-32*z));
+        g.FillRectangle(b,(int)(x-z+.5),(int)(y+.5),1,1);
+        g.FillRectangle(b,(int)(x+z+.5),(int)(y+.5),1,1);
+        g.FillRectangle(b,(int)(x+.5),(int)(y-z+.5),1,1);
+        g.FillRectangle(b,(int)(x+.5),(int)(y+z+.5),1,1);
+      }
+    }
+  }
+}
