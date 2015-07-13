03094edfd193e46a62da907f89f2d587ede0d778
David Berry <Bioniclegenius@gmail.com>
2015-07-13 15:50:43 -0500

    Star Speed Changes
    Instead of having only two speeds within a star, I made it x10. Gives a
    bit more variation on what's going on, and we shouldn't need any more
    than that. Speed changes were modified to match up, and so was scaling.

--- Definitely Not Space Invaders/star.cs
+++ Definitely Not Space Invaders/star.cs
@@ -16,20 +16,20 @@ namespace Definitely_Not_Space_Invaders {
       scrHeight=height;
       x=r.Next(0,2*width);
       y=r.Next(0,height);
-      size=r.Next(1,11);
+      size=r.Next(1,51);
     }
     public void paint(Graphics g,long mspassed=0) {
       SolidBrush b;
       double basespeed=.020;
-      double speedfactor=.003;
+      double speedfactor=.0005;
       double curspeed=basespeed+(size-1)*speedfactor;
       x-=curspeed*mspassed;
       if(x<-5) {
         x=r.Next(scrWidth,scrWidth*2);
         y=r.Next(0,scrHeight);
-        size=r.Next(1,11);
+        size=r.Next(1,51);
       }
-      for(int z=0;z<(int)(size/2.0+.5);z++) {
+      for(int z=0;z<(int)(size/10.0+.5);z++) {
         b=new SolidBrush(Color.FromArgb(196-32*z,196-32*z,196-32*z));
         g.FillRectangle(b,(int)(x-z+.5),(int)(y+.5),1,1);
         g.FillRectangle(b,(int)(x+z+.5),(int)(y+.5),1,1);
