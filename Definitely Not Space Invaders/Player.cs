9b65f5562a1a8af8f6567f0f70804955fcd3aabd
David Berry <Bioniclegenius@gmail.com>
2015-07-14 12:07:53 -0500

    Upgraded Player Graphics
    Player graphics are now based on a point-based polygon we can input,
    which it then rotates based on the player's angle. Should be much
    fancier!

--- Definitely Not Space Invaders/Player.cs
+++ Definitely Not Space Invaders/Player.cs
@@ -7,16 +7,39 @@ using System.Threading.Tasks;
 
 namespace Definitely_Not_Space_Invaders
 {
-    public class Player{
-        public int curHP, maxHP;
-        public double x, y, ang;
-
+  public class Player{
+    public int curHP, maxHP;
+    public double x, y, ang;
+    public List<PointF> origVerts;
     public Player(int scrWidth,int scrHeight) {
-        x = 20;
-        y = scrHeight/2 + 10;
-      ang=180;
+      x = 20;
+      y = scrHeight/2 + 10;
+      ang=0;
       maxHP=20;
       curHP=maxHP;
+      origVerts=new List<PointF>();
+      origVerts.Add(new PointF(8,0));
+      origVerts.Add(new PointF(7,1));
+      origVerts.Add(new PointF(1,1));
+      origVerts.Add(new PointF(-1,3));
+      origVerts.Add(new PointF(-6,3));
+      origVerts.Add(new PointF(-6,5));
+      origVerts.Add(new PointF(3,5));
+      origVerts.Add(new PointF(3,6));
+      origVerts.Add(new PointF(-7,6));
+      origVerts.Add(new PointF(-7,1));
+      origVerts.Add(new PointF(-8,1));
+      origVerts.Add(new PointF(-8,-1));
+      origVerts.Add(new PointF(-7,-1));
+      origVerts.Add(new PointF(-7,-6));
+      origVerts.Add(new PointF(3,-6));
+      origVerts.Add(new PointF(3,-5));
+      origVerts.Add(new PointF(-6,-5));
+      origVerts.Add(new PointF(-6,-3));
+      origVerts.Add(new PointF(-1,-3));
+      origVerts.Add(new PointF(1,-1));
+      origVerts.Add(new PointF(7,-1));
+      origVerts.Add(new PointF(8,0));
     }
     public void ai(int scrWidth,int scrHeight,long msPassed) {
       double spd=0;
@@ -27,14 +50,12 @@ namespace Definitely_Not_Space_Invaders
     }
     public void render(Graphics g,int scrWidth,int scrHeight,long msPassed) {
       ai(scrWidth,scrHeight,msPassed);
-      int rad=25;
-      List<Point> verts=new List<Point>();
-      verts.Add(new Point((int)(x+rad*Math.Cos(ang*Math.PI/90)+.5),
-                         (int)(y+rad*Math.Sin(ang*Math.PI/90)+.5)));
-      verts.Add(new Point((int)(x+rad*Math.Cos((ang-140)*Math.PI/90)+.5),
-                         (int)(y+rad*Math.Sin((ang-140)*Math.PI/90)+.5)));
-      verts.Add(new Point((int)(x+rad*Math.Cos((ang+140)*Math.PI/90)+.5),
-                         (int)(y+rad*Math.Sin((ang+140)*Math.PI/90)+.5)));
+      List<PointF> verts=new List<PointF>();
+      for(int z=0;z<origVerts.Count;z++) {
+        verts.Add(new PointF(
+            (float)(2*origVerts[z].X*Math.Cos(ang*Math.PI/180)-2*origVerts[z].Y*Math.Sin(ang*Math.PI/180)+x),
+            (float)(2*origVerts[z].X*Math.Sin(ang*Math.PI/180)+2*origVerts[z].Y*Math.Cos(ang*Math.PI/180)+y)));
+      }
       SolidBrush b=new SolidBrush(Color.FromArgb(255,0,0));
       g.FillPolygon(b,verts.ToArray());
     }
