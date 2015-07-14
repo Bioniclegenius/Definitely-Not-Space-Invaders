1a75d4187e34b7ec898ce8f2d65648bf8a99529f
David Berry <Bioniclegenius@gmail.com>
2015-07-14 11:46:33 -0500

    Updated Vertice Lists
    The vert arrays are now lists, which means we don't have to predict
    their size or anything - they can be dynamically added. More freedom!

--- Definitely Not Space Invaders/BasicTriangleEnemy.cs
+++ Definitely Not Space Invaders/BasicTriangleEnemy.cs
@@ -24,15 +24,15 @@ namespace Definitely_Not_Space_Invaders {
     public override void render(Graphics g,int scrWidth,int scrHeight,long msPassed) {
       ai(scrWidth,scrHeight,msPassed);
       int rad=15;
-      Point[] verts=new Point[3];
-      verts[0]=new Point((int)(x+rad*Math.Cos(ang*Math.PI/180)+.5),
-                         (int)(y+rad*Math.Sin(ang*Math.PI/180)+.5));
-      verts[1]=new Point((int)(x+rad*Math.Cos((ang-140)*Math.PI/180)+.5),
-                         (int)(y+rad*Math.Sin((ang-140)*Math.PI/180)+.5));
-      verts[2]=new Point((int)(x+rad*Math.Cos((ang+140)*Math.PI/180)+.5),
-                         (int)(y+rad*Math.Sin((ang+140)*Math.PI/180)+.5));
+      List<Point> verts=new List<Point>();
+      verts.Add(new Point((int)(x+rad*Math.Cos(ang*Math.PI/180)+.5),
+                         (int)(y+rad*Math.Sin(ang*Math.PI/180)+.5)));
+      verts.Add(new Point((int)(x+rad*Math.Cos((ang-140)*Math.PI/180)+.5),
+                         (int)(y+rad*Math.Sin((ang-140)*Math.PI/180)+.5)));
+      verts.Add(new Point((int)(x+rad*Math.Cos((ang+140)*Math.PI/180)+.5),
+                         (int)(y+rad*Math.Sin((ang+140)*Math.PI/180)+.5)));
       SolidBrush b=new SolidBrush(Color.FromArgb(0,192,63));
-      g.FillPolygon(b,verts);
+      g.FillPolygon(b,verts.ToArray());
     }
     public override void deathAnimation() {
       if(x<=-20) {//no animation if it died by just moving offscreen
--- Definitely Not Space Invaders/BossPentagon.cs
+++ Definitely Not Space Invaders/BossPentagon.cs
@@ -26,19 +26,19 @@ namespace Definitely_Not_Space_Invaders
     public override void render(Graphics g,int scrWidth,int scrHeight,long msPassed) {
       ai(scrWidth,scrHeight,msPassed);
       int rad=45;
-      Point[] verts=new Point[5];
-      verts[0]=new Point((int)(x+rad*Math.Cos(ang*Math.PI/180) + .5),
-                         (int)(y+rad*Math.Sin(ang*Math.PI/180)+.5));
-      verts[1] = new Point((int)(x + rad * Math.Cos((ang - 72) * Math.PI / 180) + .5),
-                         (int)(y + rad * Math.Sin((ang - 72) * Math.PI / 180) + .5));
-      verts[2]=new Point((int)(x+rad*Math.Cos((ang-144)*Math.PI/180)+.5),
-                         (int)(y+rad*Math.Sin((ang-144)*Math.PI/180)+.5));
-      verts[3]=new Point((int)(x+rad*Math.Cos((ang+144)*Math.PI/180)+.5),
-                         (int)(y+rad*Math.Sin((ang+144)*Math.PI/180)+.5));
-      verts[4]=new Point((int)(x+rad*Math.Cos((ang+72) *Math.PI/180)+.5),
-                         (int)(y+rad*Math.Sin((ang+72)*Math.PI/180)+.5));
+      List<Point> verts=new List<Point>();
+      verts.Add(new Point((int)(x+rad*Math.Cos(ang*Math.PI/180) + .5),
+                         (int)(y+rad*Math.Sin(ang*Math.PI/180)+.5)));
+      verts.Add(new Point((int)(x + rad * Math.Cos((ang - 72) * Math.PI / 180) + .5),
+                         (int)(y + rad * Math.Sin((ang - 72) * Math.PI / 180) + .5)));
+      verts.Add(new Point((int)(x+rad*Math.Cos((ang-144)*Math.PI/180)+.5),
+                         (int)(y+rad*Math.Sin((ang-144)*Math.PI/180)+.5)));
+      verts.Add(new Point((int)(x+rad*Math.Cos((ang+144)*Math.PI/180)+.5),
+                         (int)(y+rad*Math.Sin((ang+144)*Math.PI/180)+.5)));
+      verts.Add(new Point((int)(x+rad*Math.Cos((ang+72) *Math.PI/180)+.5),
+                         (int)(y+rad*Math.Sin((ang+72)*Math.PI/180)+.5)));
       SolidBrush b=new SolidBrush(Color.FromArgb(190,0,190));
-      g.FillPolygon(b,verts);
+      g.FillPolygon(b,verts.ToArray());
     }
     public override void deathAnimation() {
       if(x<=-20) {//no animation if it died by just moving offscreen
